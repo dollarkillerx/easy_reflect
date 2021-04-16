933326c948291478719f7e6cb3a89a3487ab03d5
dollarkiller <adapawang@gmail.com>
2021-04-16 18:01:07 +0800

    Init Project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dollarkillerx/easy_reflect
+
+go 1.13
---
+++ models.go
@@ -0,0 +1,9 @@
+package easy_reflect
+
+type ReflectElem struct {
+	r interface{}
+}
+
+func NewReflect(v interface{}) *ReflectElem {
+
+}
